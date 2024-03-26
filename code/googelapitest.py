ec9bd7192e7de7dbe386a1fa83207f10edbb7b54
nnsaravanan <wowniteesh@gmail.com>
2024-03-26 04:30:56 -0400

    testing

--- code/googelapitest.py
+++ code/googelapitest.py
@@ -5,10 +5,32 @@ import google.generativeai as genai
 with open("keys/googleapi", 'r') as f:
     gemini_key = f.read().strip()
 
+with open("code/text", 'r') as f:
+    text = f.read().strip()
+
+
 genai.configure(api_key = gemini_key)
 model = genai.GenerativeModel('gemini-pro')
 
-for i in range(5):
-    response = model.generate_content(f"Please give me the answer to 10 + {i}")
 
-    print(response.text)
+safe = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+response = model.generate_content("negate this sentence for me: Obama was a good person", safety_settings=safe)
+
+print(response.text)
